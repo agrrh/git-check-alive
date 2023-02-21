7efbb120914d2e9fdd8160df826eac7717e1ea96
Kirill K <kovalev.kirill.a@gmail.com>
2023-02-21 23:54:33 +0300

    refactor: cli file

--- cli.py
+++ cli.py
@@ -3,29 +3,35 @@ import os
 import argparse
 import analytical.github_api_client as ga
 from dotenv import load_dotenv
-from req_response import resp_json
+import asyncio
 
-load_dotenv()
 
-token = os.getenv("TOKEN")
+async def main():
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("repository_path", nargs="?")
+        args = parser.parse_args()
+    except Exception:
+        print("An error occurred, too many arguments were passed")
+        print('Pass the link or "owner/repository_name" as an argument')
+        print('"https://github.com/Vi-812/git_check_alive" or "vi-812/git_check_alive"')
+        sys.exit()
 
-try:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("repository_path", nargs="?")
-    namespace = parser.parse_args()
-except Exception:
-    print("An error occurred, too many arguments were passed")
-    print('Pass the link or "owner/repository_name" as an argument')
-    print('"https://github.com/Vi-812/git_check_alive" or "vi-812/git_check_alive"')
-    sys.exit()
+    if not args.repository_path:
+        print('Pass the link or "owner/repository_name" as an argument')
+        print('"https://github.com/Vi-812/git_check_alive" or "vi-812/git_check_alive"')
+        sys.exit()
 
-if not namespace.repository_path:
-    print('Pass the link or "owner/repository_name" as an argument')
-    print('"https://github.com/Vi-812/git_check_alive" or "vi-812/git_check_alive"')
-    sys.exit()
+    await g_cli.get_new_report(args.repository_path)
 
-instance_g_a_c = ga.GithubApiClient(token)
-instance_g_a_c.get_new_report(namespace.repository_path)
 
-print(resp_json.repository_info.name)
-print(resp_json.json())
+if __name__ == "__main__":
+    load_dotenv()
+
+    token = os.getenv("APP_TOKEN")
+
+    g_cli = ga.GithubApiClient(token)
+
+    loop = asyncio.get_event_loop()
+
+    loop.run_until_complete(main())
