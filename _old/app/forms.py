42186f54189ee871defe597fc21f9616a2cf630a
Kirill K <kovalev.kirill.a@gmail.com>
2023-02-22 21:56:26 +0300

    chore: remove old data

--- _old/app/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-
-
-class RepositoryPathForm(FlaskForm):
-    link_repository = StringField("Введите ссылку на репозиторий")
-    submit_path = SubmitField("Анализировать")
